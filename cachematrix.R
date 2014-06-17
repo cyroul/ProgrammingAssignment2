ee9c48cb8913ced36ab4f07dad641f9fd105b95a
Cyroul <cyroul.t@gmail.com>
2014-06-17 04:15:46 +0200

    Submission of the Programming Assignment 2 (Coursera)
    The 'cachematrix.R' script has been completed.
    The functions 'makeCacheMatrix' and 'cacheSolve'
    are working exactly as wanted.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##-----------------------------------------------------------------------
+##  This function creates a list of 4 functions that can interact
+##  with a cache frame containing a matrix 'x' and its inverse 'xinv'.
+##  It also inits cached data : 'x' to numeric() and 'xinv' to NULL.
+##-----------------------------------------------------------------------
+##  USAGE :  cache <- makeCacheMatrix()  : to make a list named 'cache'
+##-----------------------------------------------------------------------
 makeCacheMatrix <- function(x = matrix()) {
-
+    xinv <- NULL
+    set <- function(y) { x <<- y ; xinv <<- NULL }
+    get <- function() x
+    setsolve <- function(mat) xinv <<- mat
+    getsolve <- function() xinv
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+##-----------------------------------------------------------------------
+##  This function returns the inverse of a given matrix.
+##  It simply returns the cached matrix 'xinv' if available (not NULL),
+##  or computes it as solve('x'), and then stores it in 'xinv'.
+##-----------------------------------------------------------------------
+##  USAGE :  cache$set(a),       caches matrix 'a' in 'x'
+##  USAGE :  cache$get(),        returns the cached matrix 'x'
+##  USAGE :  cacheSolve(cache),  returns inverse of the cached matrix
+##-----------------------------------------------------------------------
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    xinv <- x$getsolve()
+    if(!is.null(xinv)) {message("getting cached data"); return(xinv) }
+    mat <- x$get()
+    xinv <- solve(mat, ...)
+    x$setsolve(xinv)
+    xinv
 }
